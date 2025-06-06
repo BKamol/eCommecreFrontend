d32f9192c9d8c80d9fade6c1029a5e15baa0be5a
BKamol <kamoljon2004mm@gmail.com>
2025-06-06 11:43:57 +0300

    Fixed form action

--- src/assets/components/NavbarSearch.jsx
+++ src/assets/components/NavbarSearch.jsx
@@ -5,8 +5,8 @@ function NavbarSearch() {
   return (
     <div className="hidden lg:flex items-center gap-2 bg-[#f0f0f0] w-80 xl:w-100 rounded-[25px] py-2 px-4 opacity-60">
         <Search color="#000000" size={20} />
-        <form action="POST">
-        <input type="text" placeholder="Search for products" className='focus:outline-none'></input>
+        <form action="/category">
+          <input type="text" placeholder="Search for products" className='focus:outline-none'></input>
         </form>
     </div>
   )
