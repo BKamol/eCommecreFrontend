cf6d1b1f4ec2fddcdd8ee6e0895416b923c95884
BKamol <kamoljon2004mm@gmail.com>
2025-06-07 12:02:51 +0300

    Added auth

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,9 +1,34 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from ..auth import (hash_password, verify_password, 
-                    create_access_token, User)
+                    create_access_token, User, UserCreate, Token)
+from database import SessionDep
+from sqlmodel import select
 
 
 router = APIRouter(prefix="/auth", tags=["auth"])
 
+@router.post("/register", reponse_model=User)
+def register(user: UserCreate, session: SessionDep):
+    existing_user = session.exec(select(User).where(User.username == user.username)).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Username already exists")
+    
+    hashed_password = hash_password(user.password)
+    db_user = User(username=user.username,
+                   hashed_password=hashed_password)
+    
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
 
+
+@router.post("/login", response_model=Token)
+def login(session: SessionDep,
+          form_data: OAuth2PasswordRequestForm = Depends()):
+    user = session.exec(select(User).where(User.username == form_data.username)).first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    access_token = create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
